bc9ccc97699be3c442cc29f28d63047635dc5e3d
nepmul <nepmul@proton.me>
2024-05-07 11:41:43 +0200

    weiterarbeit

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from tuning import tune
+from tuning import tune, umrandung_abfahren, offset
 
-path_luban="luban.nc"
-path_ender="ender.gcode"
+path_luban="Luban/Strich.nc"
+path_ender="/media/br/AC625/Strich.gcode"
 
+object_height=20
+laser_height=5
+z_hoop=2
+
+laser_speed=80
+travel_speed=5000
+
+umrandung_runden=7
+
+offset_x=1000
+offset_y=1000
+
+laser_height+=object_height
 dic_code={ #None=pass, %=nur code ersetzten, ansonsten gesammter block
     "G1": None,
     "G0": None,
@@ -14,27 +27,23 @@ dic_code={ #None=pass, %=nur code ersetzten, ansonsten gesammter block
     "M3": "M106%",
     "M5": "M107",
     "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": ""
 }
 
 startcode=[
     "G92 E0 ;reset Extruder",
     "M84 E ;Disable stepper Extruder",
     "G28 ;homing",
-    "G1 Z60.00",
-    "",
+    f"G1 Z{laser_height} F{travel_speed}",
     "",
     ""
 ]
 endcode=[
+    "M107",
+    f"G1 Z{laser_height+z_hoop*2} F3000",
+    "M84 X Y Z E ;Disable all steppers",
+    "M106",
+    "G4 P500",
     "M106 S0 ;Laser aus",
-    "G1 Z1500.0"
-    "M84 X Y Z E ;Disable all steppers"
 
 ]
 
@@ -43,12 +52,15 @@ with open(path_luban, "r") as f:
 
 file_luban=file.split("\n")
 
-file_ender=startcode
+file_ender=[]
 
 for block in file_luban:
     code = block.split(" ")[0]
 
     if block[0]==";": #Kommentare werden nicht mit übertragen
+        pass
+
+    elif "F" in block:
         pass
 
     elif code not in dic_code:
@@ -63,33 +75,35 @@ for block in file_luban:
         if "%" in new_block:
             try:
                 parameters=block.split(" ", 1)[1]#alles außer dem code
+                new_block=new_block.replace("%", " "+parameters)
             except IndexError: #keine parameter angegeben
-                parameters=""
-            new_block=new_block.replace("%", " "+parameters)
+                new_block=new_block.replace("%", "")
+
         file_ender.append(new_block)
 
-        print(f"old:{block} new:{new_block}")
+
+file_ender=tune(file_ender,laser_height, z_hoop, laser_speed, travel_speed)
+file_ender=offset(file_ender, offset_x, offset_y)
+
+umranden=umrandung_abfahren(file_ender, umrandung_runden)
+
+full_file=startcode
+
+for block in umranden:
+    full_file.append(block)
+
+for block in file_ender:
+    full_file.append(block)
 
 for block in endcode:
-    file_ender.append(block)
+    full_file.append(block)
 
-
-file_ender=tune(file_ender)
-print("\n"*7)
 
 final_file=""
-for block in file_ender:
-    print(block)
+for block in full_file:
+    #print(block)
     final_file+=block
     final_file+="\n"
 
 with open(path_ender, "w") as f:
     f.write(final_file)
-
-
-
-
-
-
-
-
--- tuning.py
+++ tuning.py
@@ -1,2 +1,76 @@
-def tune(full_gcode):
-    return full_gcode
+
+
+def tune(original_gcode, laser_height, z_hoop, laser_speed, travel_speed):
+    tuned_gcode=[]
+    for block in original_gcode:
+        new_blocks=[block]
+
+        if block=="M106":#Laser an
+            new_blocks.insert(0, f"G0 Z{laser_height}")
+            new_blocks.insert(1, f"G1 F{laser_speed}")
+            new_blocks.append("G4 P120") #1s = 1000
+
+        elif block=="M107":#Laser aus
+            new_blocks.append(f"G1 F{travel_speed}")
+            new_blocks.append(f"G0 Z{laser_height+z_hoop}")
+            new_blocks.append("G4 P200")
+
+        #elif block.split(" ")[0] == "G0":
+         #   new_blocks.insert(0, f"G0 {travel_speed}")
+          #  new_blocks.append(f"G1 {laser_speed}")
+
+        for block in new_blocks:
+            tuned_gcode.append(block)
+
+    return tuned_gcode
+
+
+def umrandung_abfahren(gcode, runden_abfahren):
+    x_max=0
+    x_min=1000
+    y_max=0
+    y_min=1000
+    for block in gcode:
+        parts_block=block.split(" ")
+        if parts_block[0] == "G1" or parts_block[0] == "G0":
+            for part in parts_block:
+                value=float(part[1:])
+
+                if part[0]=="X":
+                    if value > x_max:
+                        x_max=value
+                    if value < x_min:
+                        x_min=value
+
+                if part[0]=="Y":
+                    if value > y_max:
+                        y_max=value
+                    if value < y_min:
+                        y_min=value
+
+    #print(x_max)
+    #print(x_min)
+    #print(y_max)
+    #print(y_min)
+
+    print(f"Objektgroße: {round(x_max-x_min, 1)} * {round(y_max-y_min, 2)}")
+    ränder_abfahren1=[
+        f"G0 X{x_min} Y{y_min} F5000",
+        f"G0 Y{y_max}",
+        f"G0 X{x_max}",
+        f"G0 Y{y_min}",
+        f"G0 X{x_min} Y{y_min}",
+        f"G4 P4000"# sek warten
+    ]
+
+    ränder_abfahren=[]
+    for i in range(runden_abfahren):
+        for block in ränder_abfahren1:
+            ränder_abfahren.append(block)
+
+    #print(ränder_abfahren)
+    return ränder_abfahren
+
+
+def offset(gcode, offset_x, offset_y):
+    return gcode
